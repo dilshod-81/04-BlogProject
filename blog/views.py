60792b56b33bee712e902e11f70eb10186c98ee8
Murodkobilov <murodkobilov@inbox.ru>
2023-01-19 11:48:16 +0500

    saytimiz tayyor

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogListView(ListView):
@@ -17,3 +19,20 @@ class BlogDetailView(DetailView):
     def get_queryset(self):
         """Return Schools """
         return Post.objects.order_by('id')
+class BlogCreateView(CreateView):
+    models=Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+    def get_queryset(self):
+        """Return Schools """
+        return Post.objects.order_by('id')
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeletView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
